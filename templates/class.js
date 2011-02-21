d723dfd24affeb4d8db7d3259c02ce197605864c
Jacek Szarski <jacek@applicake.com>
2011-02-21 13:32:18 +0100

    improved Class function implementation

--- templates/class.js
+++ templates/class.js
@@ -1,15 +1,21 @@
 Class = function(instance_methods, class_methods){
   var klass = function(){
-    var prototype = klass.prototype;
+    var prototype = {};
+    for (key in klass.prototype) {
+      prototype[key] = klass.prototype[key];
+    }
     prototype.initialize.apply(prototype, arguments);
+    klass.all.push(prototype);
+    var id = klass.all.length;
+    prototype.id = function(){return id};
     return prototype;
   }
 
   klass.prototype = instance_methods;
-
   for (key in class_methods) {
     klass[key] = class_methods[key];
   }
 
+  klass.all = [];
   return klass;
 }
